a119da34110ba159f85d2c5e225972145e56448b
ShadowPrince <shadowprince47@gmail.com>
2013-10-11 11:49:38 +0300

    updated fileheads

--- Uac/Manager/User.php
+++ Uac/Manager/User.php
@@ -5,7 +5,7 @@
  * @author Vasiliy Horbachenko <shadow.prince@ya.ru>
  * @copyright 2013 Vasiliy Horbachenko
  * @version 1.0
- * @package uac
+ * @package shadowprince/uac
  *
  */
 namespace Uac\Manager;
